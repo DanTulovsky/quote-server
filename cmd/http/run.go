b448b66e805f995a12239d675359f6195ce348ae
Dan Tulovsky <dant@wetsnow.com>
2021-03-23 23:25:19 -0400

    theysaidso

--- cmd/http/run.go
+++ cmd/http/run.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/chenjiandongx/ginprom"
 	"github.com/fvbock/endless"
@@ -14,11 +18,13 @@ import (
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/semconv"
 	"go.opentelemetry.io/otel/trace"
 )
 
 const (
 	otelServiceName = "quote"
+	theySaidSoURL   = "http://quotes.rest/qod.json"
 )
 
 var (
@@ -29,6 +35,7 @@ var (
 
 func main() {
 	flag.Parse()
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	log.Printf("Starting version: %v", *version)
 
 	ls := enableOpenTelemetry()
@@ -53,19 +60,125 @@ func main() {
 		trace.GET("/", randomQuoteHandler)
 	}
 
-	endless.ListenAndServe(":4242", r)
+	endless.ListenAndServe(":8080", r)
 	// r.Run()
 }
 
 func randomQuoteHandler(c *gin.Context) {
 	ctx := c.Request.Context()
 
-	c.JSON(http.StatusOK, gin.H{
-		"quote": randomQuote(ctx),
-	})
+	quote := theysaidsoQuote(ctx)
+
+	header := "<html><body>"
+	footer := "</body></html>"
+	attribution := `
+	<span style="z-index:50;font-size:0.9em; font-weight: bold;">
+      <img src="https://theysaidso.com/branding/theysaidso.png" height="20" width="20" alt="theysaidso.com"/>
+      <a href="https://theysaidso.com" title="Powered by quotes from theysaidso.com" style="color: #ccc; margin-left: 4px; vertical-align: middle;">
+        They Said So®
+      </a>
+</span>
+	`
+
+	var sb strings.Builder
+	sb.WriteString(header)
+	sb.WriteString("<div>" + quote + "</div>")
+	sb.WriteString(attribution)
+	sb.WriteString(footer)
+
+	c.String(http.StatusOK, sb.String())
 }
 
-func randomQuote(ctx context.Context) string {
+type QuotaSearchError struct {
+	Error *qsError `json:"error"`
+}
+
+type qsError struct {
+	Code    float64 `json:"code"`
+	Message string  `json:"message"`
+}
+type QuoteSearchResult struct {
+	Status    *Total     `json:"success"`
+	Contents  *QuoteList `json:"contents"`
+	BaseURL   string     `json:"baseurl"`
+	Copyright *Copyright `json:"copyright"`
+}
+
+type Copyright struct {
+	Year float64 `json:"year"`
+	URL  string  `json:"url"`
+}
+
+type Total struct {
+	Total int `json:"total"`
+}
+
+type QuoteList struct {
+	Quotes []Quote `json:"quotes"`
+}
+
+type Quote struct {
+	Quote      string   `json:"quote"`
+	Author     string   `json:"author"`
+	Length     string   `json:"length"`
+	Tags       []string `json:"tags"`
+	Category   string   `json:"category"`
+	Language   string   `json:"language"`
+	Title      string   `json:"title"`
+	Date       string   `json:"date"`
+	Id         string   `json:"id"`
+	Background string   `json:"background"`
+	Permalink  string   `json:"permalink"`
+}
+
+func theysaidsoQuote(ctx context.Context) string {
+	_, span := tracer.Start(ctx, "theysaidsoQuote")
+	defer span.End()
+
+	span.SetAttributes(attribute.String("quote_source", theySaidSoURL))
+
+	httpClient := &http.Client{}
+	req, err := http.NewRequestWithContext(ctx, "GET", theySaidSoURL, nil)
+	if err != nil {
+		return ""
+	}
+
+	span.AddEvent("Retrieving quote")
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return err.Error()
+	}
+	defer resp.Body.Close()
+	span.AddEvent("Retrieved quote")
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err.Error()
+	}
+	span.SetAttributes(semconv.HTTPStatusCodeKey.Int(resp.StatusCode))
+
+	// body, err := io.ReadAll(resp.Body)
+	// log.Println(string(body))
+
+	var result QuoteSearchResult
+	if err := json.Unmarshal(body, &result); err != nil || result.Contents == nil {
+		// could be due to error from server
+		var errResult QuotaSearchError
+		if err := json.Unmarshal(body, &errResult); err != nil || errResult.Error == nil {
+			return fmt.Sprintf("failed to decode response from server: %v", err)
+		}
+		return errResult.Error.Message
+	}
+
+	if len(result.Contents.Quotes) > 0 {
+		return result.Contents.Quotes[0].Quote
+	}
+
+	return fmt.Sprint("did not receive any quotes from server")
+
+}
+
+func staticQuote(ctx context.Context) string {
 	// otelgin Middleware puts trace ID into context if available
 	_, span := tracer.Start(ctx, "getRandomQuote")
 	defer span.End()
